d1cf8a8218863dc7aebaa8a0376f2f091f179a8e
Carlo Sonday <angelosonday@gmail.com>
2022-10-03 22:08:49 +0800

    updates until 33b

--- apps/movies/movie_profile.py
+++ apps/movies/movie_profile.py
@@ -5,12 +5,18 @@ import dash
 from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State
 import pandas as pd
+from urllib.parse import urlparse, parse_qs
 
 from app import app
 from apps import dbconnect as db
 
 layout = html.Div(
     [
+        html.Div(
+            [
+                dcc.Store(id='movieprof_toload', storage_type='memory', data=0),
+            ]
+        ),
         html.H2("Movie Details"),
         html.Hr(),
         dbc.Row(
@@ -75,12 +81,16 @@ layout = html.Div(
 @app.callback(
     [
         Output('movieprof_genre', 'options'),
+        Output('movieprof_toload', 'data'),
     ],
     [
         Input('url', 'pathname')
+    ],
+    [
+        State('url', 'search')
     ]
 )
-def movieprof_loaddropdown(pathname):
+def movieprof_loaddropdown(pathname, search):
     
     if pathname == '/movies/movie_profile':
         sql = """
@@ -92,11 +102,15 @@ def movieprof_loaddropdown(pathname):
         cols = ['label', 'value']
         df = db.querydatafromdatabase(sql, values, cols)
         genre_opts = df.to_dict('records')
+        
+        parsed = urlparse(search)
+        mode = parse_qs(parsed.query)['mode'][0]
+        to_load = 1 if mode == 'edit' else 0
     
     else:
         raise PreventUpdate
 
-    return [genre_opts]
+    return [genre_opts, to_load]
 
 
 @app.callback(
@@ -113,11 +127,13 @@ def movieprof_loaddropdown(pathname):
         State('movieprof_title', 'value'),
         State('movieprof_releasedate', 'date'),
         State('movieprof_genre', 'value'),
+        State('url', 'search'),
     ]
 )
 def movieprof_submitprocess(submitbtn, closebtn,
                             
-                            title, releasedate, genre):
+                            title, releasedate, genre,
+                            search):
     ctx = dash.callback_context
     if ctx.triggered:
         # eventid = name of the element that caused the trigger
@@ -146,20 +162,30 @@ def movieprof_submitprocess(submitbtn, closebtn,
         
         # else, save to db and have feedback
         else:
-            # save to db
-            sqlcode = """INSERT INTO movies(
-                movie_name,
-                genre_id,
-                movie_release_date,
-                movie_delete_ind
-            )
-            VALUES (%s, %s, %s, %s)
-            """
-            values = [title, genre, releasedate, False]
-            db.modifydatabase(sqlcode, values)
+            parsed = urlparse(search)
+            mode = parse_qs(parsed.query)['mode'][0]
             
-            feedbackmessage = "Movie has been saved."
-            okay_href = '/movies'
+            if mode == 'add':
+                # save to db
+                sqlcode = """INSERT INTO movies(
+                    movie_name,
+                    genre_id,
+                    movie_release_date,
+                    movie_delete_ind
+                )
+                VALUES (%s, %s, %s, %s)
+                """
+                values = [title, genre, releasedate, False]
+                db.modifydatabase(sqlcode, values)
+                
+                feedbackmessage = "Movie has been saved."
+                okay_href = '/movies'
+            
+            elif mode == 'edit':
+                raise PreventUpdate # to update later :)
+            
+            else:
+                raise PreventUpdate
     
     elif eventid == 'movieprof_closebtn' and closebtn:
         pass
@@ -168,3 +194,44 @@ def movieprof_submitprocess(submitbtn, closebtn,
         raise PreventUpdate
     
     return [openmodal, feedbackmessage, okay_href]
+
+
+@app.callback(
+    [
+        Output('movieprof_title', 'value'),
+        Output('movieprof_releasedate', 'date'),
+        Output('movieprof_genre', 'value'),
+    ],
+    [
+        Input('movieprof_toload', 'modified_timestamp'),
+    ],
+    [
+        State('movieprof_toload', 'data'),
+        State('url', 'search'),
+    ]
+)
+def loadmoviedetails(timestamp, to_load, search):
+    if to_load == 1:
+        # 1. query the movie details from the database
+        sql = """SELECT movie_name, genre_id, movie_release_date
+        FROM movies
+        WHERE movie_id = %s"""
+        
+        parsed = urlparse(search)
+        movieid = parse_qs(parsed.query)['id'][0]
+        
+        val = [movieid]
+        colnames = ['title', 'genre', 'reldate']
+        
+        df = db.querydatafromdatabase(sql, val, colnames)
+                    
+        
+        # 2. load the values to the interface
+        title = df['title'][0]
+        genre = df['genre'][0]
+        reldate = df['reldate'][0]
+        
+        return [title, reldate, genre]
+    
+    else:
+        raise PreventUpdate
--- apps/movies/movies_home.py
+++ apps/movies/movies_home.py
@@ -1,11 +1,14 @@
+from logging import warning
 from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
 import dash
 from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output, State
 import pandas as pd
 
 from app import app
+from apps import dbconnect as db
 
 layout = html.Div(
     [
@@ -16,7 +19,7 @@ layout = html.Div(
                 dbc.CardHeader(html.H4("Movie Management")),
                 dbc.CardBody(
                     [
-                        dbc.Button('Add Movie', color="secondary", href='/movies/movie_profile'),
+                        dbc.Button('Add Movie', color="secondary", href='/movies/movie_profile?mode=add'),
                         html.Hr(),
                         html.Div(
                             [
@@ -45,3 +48,60 @@ layout = html.Div(
         )
     ]
 )
+
+
+
+@app.callback(
+    [
+        Output('movie_movielist', 'children'),
+    ],
+    [
+        Input('url', 'pathname'),
+        Input('movie_name_filter', 'value'),
+    ]
+)
+def updatemovielist(pathname, searchterm):
+    if pathname == '/movies':
+        # 1. query the relevant records
+        sql = """SELECT movie_name, genre_name, movie_id
+                FROM movies m
+                    INNER JOIN genres g ON m.genre_id = g.genre_id
+                WHERE NOT movie_delete_ind"""
+        val = []
+        colnames = ['Movie Title', 'Genre', 'ID']
+        
+        if searchterm:
+            sql += """ AND movie_name ILIKE %s"""
+            val += [f"%{searchterm}%"]
+        
+        
+        movies = db.querydatafromdatabase(sql, val, colnames)
+        
+        # 2. create the table and add it to the interface
+        
+        if movies.shape[0]:
+            # add the buttons with the respective href
+            buttons = []
+            for movieid in movies['ID']:
+                buttons += [
+                    html.Div(
+                        dbc.Button('Edit/Delete', href=f"/movies/movie_profile?mode=edit&id={movieid}",
+                               size='sm', color='warning'),
+                        style={'text-align': 'center'}
+                    )
+                ]
+            
+            # we add the buttons to the movies table
+            movies['Action'] = buttons
+            
+            # remove the ID column
+            movies.drop('ID', axis=1, inplace=True)
+            
+            table = dbc.Table.from_dataframe(movies, striped=True, bordered=True, hover=True, size='sm')
+            
+            return [table]
+        else:
+            return ["There are no records that match the search term."]
+        
+    else:
+        raise PreventUpdate
